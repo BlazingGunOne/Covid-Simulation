fcded23066adb6d893be6c107b73bac3d1c2d42e
Swapnil Ghosh <gswapnil100@gmail.com>
2020-12-04 21:47:53 +0530

    Cases vs time plots

--- CoviDSim.py
+++ CoviDSim.py
@@ -13,17 +13,29 @@ print((H.nodes(data = True)))
 print("\n")
 print((H.edges(data = True)))
 
+S = []
+I = []
+R = []
 
 def update(num, layout, G, ax):
     ax.clear()
     color = []
+    s = 0
+    i = 0
+    r = 0
     for node in G.nodes():
         if G.nodes[node]['state'] == 'I':
             color.append('r')
+            i += 1
         elif G.nodes[node]['state'] == 'S':
             color.append('b')
+            s += 1
         elif G.nodes[node]['state'] == 'R':
             color.append('g')
+            r += 1
+    S.append(s)
+    I.append(i)
+    R.append(r)
 
     nx.draw(G, pos = layout, node_color = color, with_labels = True, ax = ax)
 
@@ -41,7 +53,16 @@ def animate(G):
     layout = nx.spring_layout(G)
 
     ani = animation.FuncAnimation(fig, update, interval=750, fargs=(layout, G, ax))
-
+    # ani.save('animation_1.gif', writer='imagemagick')
     plt.show(1)
 
 animate(H)
+
+print('\n')
+print(S)
+print(I)
+print(R)
+plt.plot(S)
+plt.plot(I)
+plt.plot(R)
+plt.show(1)
