634c8f44f58927a3fe94f662a329ebe8e67454ce
Swapnil Ghosh <gswapnil100@gmail.com>
2020-12-04 16:52:22 +0530

    Simulation Fixed

--- CoviDNetwork.py
+++ CoviDNetwork.py
@@ -1,16 +1,27 @@
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
 import pylab
 pylab.ion()
 
-# random_pos = nx.random_layout(G, seed=1)
-# pos = nx.spring_layout(G, pos=random_pos)
 
 def add_cluster(network, size, deg, status):
     h = nx.Graph()
-    node_list = list(range(len(network.nodes()), len(network.nodes()) + size))
-    h.add_nodes_from(node_list, state = status)
+    node_list = []
+    for n in range(len(network.nodes()), len(network.nodes()) + size):
+        viral_load = 0
+        if(status == 'I'):
+            viral_load = round(random.uniform(0, 1), 2)
+        attr = {
+            'state' : status,
+            'immunity' : round(random.choice([-1, 1])*random.uniform(0.1, 1), 2),
+            'load' : viral_load
+        }
+        node_list.append((n, attr))
+    # print(node_list)
+    h.add_nodes_from(node_list)
     for u in h.nodes():
         for v in h.nodes():
             randno = random.uniform(0, 1)
@@ -19,63 +30,68 @@ def add_cluster(network, size, deg, status):
                 h.add_edge(u, v, weight = round(random.uniform(0.5, 1), 2))
     return nx.disjoint_union(network, h)
 
-def add_edges(h, index1, index2, index3, index4, p):
-    for u in h.nodes():
+
+def add_edges(G, index1, index2, index3, index4, p):
+    for u in G.nodes():
         if index1 <= u and index2 >= u:
-            for v in h.nodes():
+            for v in G.nodes():
                 if index3 <= v and index4 >= v:
                     randno = random.uniform(0, 1)
                     if randno <= p:
-                        h.add_edge(u, v, weight = round(random.uniform(0, 0.7), 2))
-    return h
+                        G.add_edge(u, v, weight = round(random.uniform(0, 0.7), 2))
+
 
 def infect(G):
     for edge in G.edges():
-        if G[edge[0]][edge[1]]['weight'] < 0.5:
-            continue
         if G.nodes[edge[0]]['state'] == 'I' and G.nodes[edge[1]]['state'] == 'S':
-            G.nodes[edge[1]]['state'] = 'I'    
+            virus = G.nodes[edge[0]]['load']*(G[edge[0]][edge[1]]['weight']*2)*(2**(-1-G.nodes[edge[1]]['immunity']))
+            if virus > 0.35:
+                G.nodes[edge[1]]['state'] = 'I'
+                G.nodes[edge[1]]['load'] = min(virus, 1)    
         elif G.nodes[edge[0]]['state'] == 'S' and G.nodes[edge[1]]['state'] == 'I':
-            G.nodes[edge[0]]['state'] = 'I'
+            virus = G.nodes[edge[1]]['load']*(G[edge[0]][edge[1]]['weight']*2)*(2**(-1-G.nodes[edge[0]]['immunity']))
+            if virus > 0.25:
+                G.nodes[edge[0]]['state'] = 'I'
+                G.nodes[edge[0]]['load'] = min(virus, 1)    
 
-def spread(G):
-    infect(G)
+
+def remove(G):
+    for node in G.nodes():
+        if G.nodes[node]['state'] == 'I':
+            G.nodes[node]['load'] -= 0.1*G.nodes[node]['immunity']
+            if G.nodes[node]['load'] >= 1 or G.nodes[node]['load'] <= 0.2:
+                G.nodes[node]['state'] = 'R'
+
+
+################################################################################################################
+
+def spread(G, layout):
     color = []
     for node in G.nodes():
         if G.nodes[node]['state'] == 'I':
             color.append('r')
-        else:
+        elif G.nodes[node]['state'] == 'S':
             color.append('b')
+        elif G.nodes[node]['state'] == 'R':
+            color.append('g') 
     # print(G.nodes.data('state'))
     fig = pylab.figure()
-    nx.draw(G, node_color = color, with_labels = True)
+    nx.draw(G, pos = layout, node_color = color, with_labels = True)
+    remove(G)
+    infect(G)
     return fig
 
-def run(G):
-    n = 5
+def run(G, n):
+    layout = nx.spring_layout(G)
     pylab.show()
     for i in range(n):
-        fig = spread(G)
+        fig = spread(G, layout)
         fig.canvas.draw()
         pylab.draw()
         plt.pause(2)
         pylab.close(fig)
 
-H = nx.Graph()
-
-H = add_cluster(H, 5, 3, 'S')
-H = add_cluster(H, 4, 3, 'S')
-H = add_cluster(H, 2, 2, 'I')
-
-H = add_edges(H, 0, 4, 5, 8, 0.25)
-H = add_edges(H, 9, 10, 5, 8, 0.25)
-H = add_edges(H, 0, 4, 9, 10, 0.25)
-
-print((H.nodes(data = True)))
-print("\n")
-print((H.edges(data = True)))
-
 # nx.draw(H)
 # plt.show(20)
 
-run(H)
+# run(H, 5)
---
+++ CoviDSim.py
@@ -0,0 +1,58 @@
+from CoviDNetwork import *
+
+H = nx.Graph()
+
+H = add_cluster(H, 10, 8, 'S')
+H = add_cluster(H, 10, 7, 'S')
+H = add_cluster(H, 5, 3, 'I')
+
+add_edges(H, 0, 9, 9, 19, 0.25)
+add_edges(H, 0, 19, 20, 24, 0.4)
+
+print((H.nodes(data = True)))
+print("\n")
+print((H.edges(data = True)))
+
+
+
+
+
+# TO DOS:
+# Check everything
+# Add immunity, viral load and edge weight distribution...Done
+# Implement Spread based on immunity, viral load and edge weight...Done
+# Go from I -> R...Done
+# Sort Animation...Done
+# Reduce speed of animation....Done ig
+
+def update(num, layout, G, ax):
+    ax.clear()
+    color = []
+    for node in G.nodes():
+        if G.nodes[node]['state'] == 'I':
+            color.append('r')
+        elif G.nodes[node]['state'] == 'S':
+            color.append('b')
+        elif G.nodes[node]['state'] == 'R':
+            color.append('g')
+
+    nx.draw(G, pos = layout, node_color = color, with_labels = True, ax = ax)
+
+    # Set the title
+    ax.set_title("Frame {}".format(num))
+    remove(G)
+    infect(G)
+
+
+def animate(G):
+
+    # Build plot
+    fig, ax = plt.subplots(figsize=(6,4))
+
+    layout = nx.spring_layout(G)
+
+    ani = animation.FuncAnimation(fig, update, interval=500, fargs=(layout, G, ax))
+
+    plt.show(1)
+
+animate(H)
